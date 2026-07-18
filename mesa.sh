1491f85a65c9c0f7c69578c686dd85d91062a8a3
Time And Time Studio <nok.craft.studio@gmail.com>
2026-07-18 17:32:57 +0700

    Update mesa.sh

--- mesa.sh
+++ mesa.sh
@@ -8,8 +8,7 @@ export PATH="$FFMPEG_PREFIX/bin:$MESA_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$FFMPEG_PREFIX/lib:$MESA_PREFIX/lib64"
 
 export LIBGL_DRIVERS_PATH="$MESA_PREFIX/lib64/dri"
-
-export LIBVA_DRIVERS_PATH="$MESA_PREFIX/lib64/dri"
+export LIBVA_DRIVERS_PATH="$MESA_PREFIX/lib64/dri:/usr/lib64/dri:$LIBVA_DRIVERS_PATH"
 
 # ให้ Vulkan หา AMD + Intel ICD เอง
 export VK_DRIVER_FILES="$MESA_PREFIX/share/vulkan/icd.d"
